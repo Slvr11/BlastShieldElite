e9ea00f60e7d809e55035d2114aa824d89df6891
Slvr99 <bsawii@gmail.com>
2019-04-27 19:31:30 -0400

    Add project files.

---
+++ BlastShieldElite/BlastShield.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InfinityScript;
+
+namespace BlastShield
+{
+    public class BlastShield : BaseScript
+    {
+        public override void OnPlayerDamage(Entity player, Entity inflictor, Entity attacker, int damage, int dFlags, string mod, string weapon, Vector3 point, Vector3 dir, string hitLoc)
+        {
+            if (dFlags == 16)//16 is Stun? if dFLags count as idFlags. If not, use: if weapon == "concussion_grenade_mp"
+            {
+                if (player.Call<bool>("hasperk", "specialty_stun_resistance"))
+                {
+                    //player = stunned player
+                    AfterDelay(1000, () =>//We'll need to fix this up. Time this out for a safe cancel.
+                        {
+                            player.Call("stopshellshock");
+                            //player.Call("fadeoutshellshock");Use if above is obvious
+                        });
+                }
+            }
+        }
+    }
+}
